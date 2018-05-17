c33d00f2c370a707e00807cc23b7095605e890a4
Mehmet İnce <mehmet.ince@tikle.com>
2018-05-17 13:37:20 +0300

    Singleton DP

---
+++ CreationalPatterns/SingletonDP/Vodafone.js
@@ -0,0 +1,30 @@
+var Vodafone = (function () {
+    var _line;
+	var _vodafone = function () 
+				{
+						var _name = "VODAFONE"; // private instance data
+						var _number = "542";    // private instance data
+
+						this.GetName = function() {return(_name);}     // methods with access to private variable
+						this.GetNumber = function() {return(_number);}     // methods with access to private variable
+						
+				}
+ 
+    return {
+        GetInstance: function () {
+            if (!_line) {
+                _line = new _vodafone();
+            }
+            return _line;
+        }
+    };
+})();
+ 
+function run() {
+ 
+		var line = Vodafone.GetInstance();
+        var name = line.GetName();
+        var number = line.GetNumber();
+
+        console.log("Name: " + name + ", number: " + number);
+}
